458e358963336920c4ff829032970405ebbda143
iugo <iugogogo@gmail.com>
2024-09-06 10:57:00 +0800

    feat: 新增代理功能, 完善测试

--- src/get-access-token.test.ts
+++ src/get-access-token.test.ts
@@ -8,8 +8,6 @@ const agentSecret = Deno.env.get("QY_WECHAT_QRLOGIN_SECRET")!;
 Deno.test("getAccessToken", async () => {
   const res = await getAccessToken(corpID, agentSecret);
   assertEquals(res.errcode, 0);
-  if ("access_token" in res) {
-    assertEquals(typeof res.access_token, "string");
-  }
+  assertEquals(typeof res.access_token, "string");
   // console.log(res);
 });
--- src/get-jsapi-ticket.ts
+++ src/get-jsapi-ticket.ts
@@ -1,18 +1,25 @@
+import { QywechatRes } from "./basetypes.ts";
+
 /**
  * 获取企业的 jsapi_ticket
  * @param accessToken 企业微信的 access_token
  * @returns
  */
-export async function getCorpJsapiTicket(accessToken: string): Promise<{
-  errcode: number;
-  errmsg: string;
-  /** 生成签名所需的jsapi_ticket，最长为512字节 */
-  ticket: string;
-  /** 凭证的有效时间（秒） */
-  expires_in: number;
-}> {
+export async function getCorpJsapiTicket(
+  accessToken: string,
+  { proxy }: { proxy?: string }
+): Promise<
+  QywechatRes<{
+    /** 生成签名所需的jsapi_ticket，最长为512字节 */
+    ticket: string;
+    /** 凭证的有效时间（秒） */
+    expires_in: number;
+  }>
+> {
   const res = await fetch(
-    `https://qyapi.weixin.qq.com/cgi-bin/get_jsapi_ticket?access_token=${accessToken}`
+    `https://${
+      proxy ?? ""
+    }qyapi.weixin.qq.com/cgi-bin/get_jsapi_ticket?access_token=${accessToken}`
   );
   const data = await res.json();
   return data;
@@ -23,16 +30,21 @@ export async function getCorpJsapiTicket(accessToken: string): Promise<{
  * @param accessToken 应用的调用接口凭证
  * @returns
  */
-export async function getAgentJsapiTicket(accessToken: string): Promise<{
-  errcode: number;
-  errmsg: string;
-  /** 生成签名所需的jsapi_ticket，最长为512字节 */
-  ticket: string;
-  /** 凭证的有效时间（秒） */
-  expires_in: number;
-}> {
+export async function getAgentJsapiTicket(
+  accessToken: string,
+  { proxy }: { proxy?: string }
+): Promise<
+  QywechatRes<{
+    /** 生成签名所需的jsapi_ticket，最长为512字节 */
+    ticket: string;
+    /** 凭证的有效时间（秒） */
+    expires_in: number;
+  }>
+> {
   const res = await fetch(
-    `https://qyapi.weixin.qq.com/cgi-bin/ticket/get?access_token=${accessToken}&type=agent_config`
+    `https://${
+      proxy ?? ""
+    }qyapi.weixin.qq.com/cgi-bin/ticket/get?access_token=${accessToken}&type=agent_config`
   );
   const data = await res.json();
   return data;
