fbc7d10efb49bc48ea15a21f4313f73d32f5fdac
iugo <iugogogo@gmail.com>
2024-09-06 10:38:38 +0800

    chore: 完善类型注释

--- src/basetypes.ts
+++ src/basetypes.ts
@@ -1,3 +1,9 @@
+/**
+ * 企业微信 API 返回值类型
+ *
+ * 当前 TS 判断不足, 导致无法直接通过 errcode 判断是否成功,
+ * 需要通过 `in` 判断是否存在 T 中的属性来判断是否成功.
+ */
 export type QywechatRes<T extends object> =
   | ({
       /** 出错返回码，为0表示成功 */
