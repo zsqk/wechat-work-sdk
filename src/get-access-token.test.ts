2d7169a53e910754a0456666c49eafe399e712de
iugo <iugogogo@gmail.com>
2024-09-06 09:45:26 +0800

    feat: 新增 getAccessToken

---
+++ src/get-access-token.test.ts
@@ -0,0 +1,13 @@
+import { assertEquals } from "@std/assert";
+import { getAccessToken } from "./get-access-token.ts";
+
+const corpID = Deno.env.get("QY_WECHAT_CORPID")!;
+const agentID = Deno.env.get("QY_WECHAT_QRLOGIN_AGENTID")!;
+const agentSecret = Deno.env.get("QY_WECHAT_QRLOGIN_SECRET")!;
+
+Deno.test("getAccessToken", async () => {
+  const res = await getAccessToken(corpID, agentSecret);
+  assertEquals(res.errcode, 0);
+  assertEquals(typeof res.access_token, "string");
+  // console.log(res);
+});
