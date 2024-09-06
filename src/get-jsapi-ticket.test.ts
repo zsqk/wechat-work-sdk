a52380481c92dd865f89d146d7d295268cb330ce
iugo <iugogogo@gmail.com>
2024-09-06 15:38:16 +0800

    feat: genSignature

--- src/get-jsapi-ticket.test.ts
+++ src/get-jsapi-ticket.test.ts
@@ -1,6 +1,10 @@
 import { assertEquals } from "@std/assert";
 import { getAccessToken } from "./get-access-token.ts";
-import { getAgentJsapiTicket, getCorpJsapiTicket } from "./get-jsapi-ticket.ts";
+import {
+  genSignature,
+  getAgentJsapiTicket,
+  getCorpJsapiTicket,
+} from "./get-jsapi-ticket.ts";
 
 const corpID = Deno.env.get("QY_WECHAT_CORPID")!;
 const agentID = Deno.env.get("QY_WECHAT_QRLOGIN_AGENTID")!;
@@ -21,4 +25,16 @@ Deno.test("getAgentJsapiTicket", async () => {
   // console.log("res", res);
   assertEquals(res.errcode, 0);
   assertEquals(typeof res.ticket, "string");
+});
+
+Deno.test("genSignature", async () => {
+  const { ticket } = await getCorpJsapiTicket(access_token!, { proxy });
+  const { timestamp, nonceStr, signature } = await genSignature(
+    ticket!,
+    "https://www.baidu.com/abc?a=%E5%A5%BD&b=2"
+  );
+  assertEquals(typeof timestamp, "number");
+  assertEquals(typeof nonceStr, "string");
+  assertEquals(typeof signature, "string");
+  // console.log({ ticket, timestamp, nonceStr, signature });
 });
