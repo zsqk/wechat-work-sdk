f4a8452f3ab9e713e46adb668b76ab37ef574bf2
iugo <iugogogo@gmail.com>
2024-09-06 10:56:39 +0800

    rf: 调整类型减少不必要的判断

--- src/basetypes.ts
+++ src/basetypes.ts
@@ -2,7 +2,7 @@
  * 企业微信 API 返回值类型
  *
  * 当前 TS 判断不足, 导致无法直接通过 errcode 判断是否成功,
- * 需要通过 `in` 判断是否存在 T 中的属性来判断是否成功.
+ * 所以将类型额外新增了 Record<keyof T, undefined> 来避免做不必要的类型判断.
  */
 export type QywechatRes<T extends object> =
   | ({
@@ -11,9 +11,9 @@ export type QywechatRes<T extends object> =
       /** 返回码提示语 */
       errmsg: "ok";
     } & T)
-  | {
+  | ({
       /** 出错返回码，非0表示调用失败 */
       errcode: Exclude<number, 0>;
       /** 返回码提示语 */
       errmsg: Exclude<string, "ok">;
-    };
+    } & Record<keyof T, undefined>);
