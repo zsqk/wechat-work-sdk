4dcf07997fd2b9e1fa662f184e73384c39b95892
iugo <iugogogo@gmail.com>
2025-11-07 16:22:35 +0800

    feat: enhance getFields function to support proxy configuration and improve test setup

--- src/hr/get-fields.test.ts
+++ src/hr/get-fields.test.ts
@@ -1,3 +1,4 @@
+import { setVerbose } from "../constants.ts";
 import { getAccessToken } from "../get-access-token.ts";
 import { debug } from "../log.ts";
 import { getFields } from "./get-fields.ts";
@@ -5,13 +6,13 @@ import { getFields } from "./get-fields.ts";
 let accessToken = "";
 
 Deno.test.beforeAll(async () => {
-  debug("Starting getFields tests...");
+  setVerbose(true);
   const corpID = Deno.env.get("QY_WECHAT_CORPID")!;
   const agentSecret = Deno.env.get("QY_WECHAT_QRLOGIN_SECRET")!;
   accessToken = (await getAccessToken(corpID, agentSecret)).access_token!;
 });
 
 Deno.test("getFields", async () => {
-  const response = await getFields({ accessToken });
+  const response = await getFields({ accessToken, proxy: 'v2.fixedip.zsqk.com.cn' });
   debug("response:", response);
 });
--- src/hr/get-fields.ts
+++ src/hr/get-fields.ts
@@ -1,8 +1,8 @@
 // 获取员工字段配置
 // https://developer.work.weixin.qq.com/document/path/99131
 // GET https://qyapi.weixin.qq.com/cgi-bin/hr/get_fields?access_token=ACCESS_TOKEN
-import type { SDKOptions } from "../basetypes.ts";
-import { debug as defaultDebug } from "../log.ts";
+import type { SDKOptions } from '../basetypes.ts';
+import { debug as defaultDebug } from '../log.ts';
 
 // TODO: 完善返回类型
 export async function getFields({
@@ -10,12 +10,13 @@ export async function getFields({
   proxy,
   debug = defaultDebug,
 }: SDKOptions): Promise<any> {
-  const res = await fetch(
-    `https://${
-      proxy ? `${proxy}/` : ""
-    }qyapi.weixin.qq.com/cgi-bin/get_jsapi_ticket?access_token=${accessToken}`
-  );
+  const domain = 'qyapi.weixin.qq.com';
+  const path = '/cgi-bin/hr/get_fields';
+  const url = new URL(`https://${proxy ? `${proxy}/` : ''}${domain}${path}`);
+  url.searchParams.set('access_token', accessToken);
+  debug('getFields url:', url);
+  const res = await fetch(url);
   const data = await res.json();
-  debug("getFields response:", data);
+  debug('getFields response:', data);
   return data;
 }
